acb4b49900dc87f613dcd09d8f7cfb66f2c8d847
Leo Kott <121113067+violetstar701@users.noreply.github.com>
2023-01-26 14:11:01 -0500

    dashboard session

--- login/dashboard.php
+++ login/dashboard.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require_once __DIR__ . '/includes/common.php';
 require_once __DIR__ . '/includes/database.php';
@@ -149,8 +150,8 @@ require_once __DIR__ . '/includes/init-session.php';
         <?PHP } ?>
 
         <?PHP
-                  if ($_SESSION['usertype'] != 'Client') {
-                    ?>
+        if ($_SESSION['usertype'] != 'Client') {
+          ?>
 
         <!-- //market-->
         <div class="market-updates">
@@ -252,98 +253,98 @@ require_once __DIR__ . '/includes/init-session.php';
         <!-- //graph-->
 
         <!-- tasks 
-                  <div class="agile-last-grids">
-                          <div class="col-md-4 agile-last-left agile-last-middle">
-                      <div class="agile-last-grid">
-                        <div class="area-grids-heading">
-                          <h3>Daily</h3>
+                    <div class="agile-last-grids">
+                            <div class="col-md-4 agile-last-left agile-last-middle">
+                        <div class="agile-last-grid">
+                          <div class="area-grids-heading">
+                            <h3>Daily</h3>
+                          </div>
+                          <div id="graph8"></div>
+                          <script>
+                          /* data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type */
+                          var day_data = [
+                            {"period": "2019-10-18", "licensed": 12},
+                            {"period": "2019-09-19", "licensed": 3},
+                            {"period": "2019-09-20", "licensed": 4},
+                            {"period": "2019-09-21", "licensed": 7},
+                            {"period": "2019-09-22", "licensed": 9},
+                            {"period": "2019-09-23", "licensed": 4},
+                            {"period": "2019-09-24", "licensed": 1},
+                            {"period": "2019-09-25", "licensed": 3},
+                            {"period": "2019-09-26", "licensed": 9},
+                            {"period": "2019-09-27", "licensed": 4}
+                          ];
+                          Morris.Bar({
+                            element: 'graph8',
+                            data: day_data,
+                            xkey: 'period',
+                            ykeys: ['licensed'],
+                            labels: ['No. of Projects'],
+                            xLabelAngle: 60
+                          });
+                          </script>
                         </div>
-                        <div id="graph8"></div>
-                        <script>
-                        /* data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type */
-                        var day_data = [
-                          {"period": "2019-10-18", "licensed": 12},
-                          {"period": "2019-09-19", "licensed": 3},
-                          {"period": "2019-09-20", "licensed": 4},
-                          {"period": "2019-09-21", "licensed": 7},
-                          {"period": "2019-09-22", "licensed": 9},
-                          {"period": "2019-09-23", "licensed": 4},
-                          {"period": "2019-09-24", "licensed": 1},
-                          {"period": "2019-09-25", "licensed": 3},
-                          {"period": "2019-09-26", "licensed": 9},
-                          {"period": "2019-09-27", "licensed": 4}
-                        ];
-                        Morris.Bar({
-                          element: 'graph8',
-                          data: day_data,
-                          xkey: 'period',
-                          ykeys: ['licensed'],
-                          labels: ['No. of Projects'],
-                          xLabelAngle: 60
-                        });
-                        </script>
                       </div>
-                    </div>
             
-                    <div class="col-md-4 agile-last-left">
-                      <div class="agile-last-grid">
-                        <div class="area-grids-heading">
-                          <h3>Monthly</h3>
-                        </div>
-                        <div id="graph7"></div>
-                        <script>
-                        // This crosses a DST boundary in the UK.
-                        Morris.Area({
-                          element: 'graph7',
-                          data: [
-                          {x: '2019-04', y: 25},
-                          {x: '2019-05', y: 3},
-                          {x: '2019-06', y: 15},
-                          {x: '2019-07', y: 35},
-                          {x: '2019-08', y: 7},
-                          {x: '2019-09', y: 38}
-                          ],
-                          xkey: 'x',
-                          ykeys: ['y'],
-                          labels: ['No. of Projects']
-                        });
-                        </script>
+                      <div class="col-md-4 agile-last-left">
+                        <div class="agile-last-grid">
+                          <div class="area-grids-heading">
+                            <h3>Monthly</h3>
+                          </div>
+                          <div id="graph7"></div>
+                          <script>
+                          // This crosses a DST boundary in the UK.
+                          Morris.Area({
+                            element: 'graph7',
+                            data: [
+                            {x: '2019-04', y: 25},
+                            {x: '2019-05', y: 3},
+                            {x: '2019-06', y: 15},
+                            {x: '2019-07', y: 35},
+                            {x: '2019-08', y: 7},
+                            {x: '2019-09', y: 38}
+                            ],
+                            xkey: 'x',
+                            ykeys: ['y'],
+                            labels: ['No. of Projects']
+                          });
+                          </script>
 
+                        </div>
                       </div>
-                    </div>
         
-                    <div class="col-md-4 agile-last-left agile-last-right">
-                      <div class="agile-last-grid">
-                        <div class="area-grids-heading">
-                          <h3>Yearly</h3>
-                        </div>
-                        <div id="graph9"></div>
-                        <script>
-                        var day_data = [
-                          {"elapsed": "2011", "value": 250},
-                          {"elapsed": "2012", "value": 25},
-                          {"elapsed": "2013", "value": 150},
-                          {"elapsed": "2014", "value": 300},
-                          {"elapsed": "2015", "value": 28},
-                          {"elapsed": "2016", "value": 50},
-                          {"elapsed": "2017", "value": 320},
-                          {"elapsed": "2018", "value": 250}
-                        ];
-                        Morris.Line({
-                          element: 'graph9',
-                          data: day_data,
-                          xkey: 'elapsed',
-                          ykeys: ['value'],
-                          labels: ['No. of Projects'],
-                          parseTime: false
-                        });
-                        </script>
+                      <div class="col-md-4 agile-last-left agile-last-right">
+                        <div class="agile-last-grid">
+                          <div class="area-grids-heading">
+                            <h3>Yearly</h3>
+                          </div>
+                          <div id="graph9"></div>
+                          <script>
+                          var day_data = [
+                            {"elapsed": "2011", "value": 250},
+                            {"elapsed": "2012", "value": 25},
+                            {"elapsed": "2013", "value": 150},
+                            {"elapsed": "2014", "value": 300},
+                            {"elapsed": "2015", "value": 28},
+                            {"elapsed": "2016", "value": 50},
+                            {"elapsed": "2017", "value": 320},
+                            {"elapsed": "2018", "value": 250}
+                          ];
+                          Morris.Line({
+                            element: 'graph9',
+                            data: day_data,
+                            xkey: 'elapsed',
+                            ykeys: ['value'],
+                            labels: ['No. of Projects'],
+                            parseTime: false
+                          });
+                          </script>
 
+                        </div>
                       </div>
+                      <div class="clearfix"> </div>
                     </div>
-                    <div class="clearfix"> </div>
-                  </div>
-                <!-- //tasks -->
+                  <!-- //tasks -->
 
         <?PHP } ?>
 
